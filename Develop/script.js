74cc35c589d240cb8b824002da4e6e28ed7769fa
mattgaither <benjamin.gaither@shipt.com>
2022-04-21 14:39:09 -0500

    forgot numberArr and added it to the prompt()

--- Develop/script.js
+++ Develop/script.js
@@ -1,36 +1,41 @@
+// Assignment code here
 var characterLength = 8;
 var choiceArr = [];
 
 
-
-
-// Assignment code here
-
 // prompts to generate passcode
 function getPrompts() {
+  // This resets the choiceArr so it does not save and repaaet the function on the next generate button click
+  choiceArr = [];
+
   characterLength = parseInt(prompt("How many characters would you like your password to be? (8 - 128 characters)"));
 
     if(isNaN(characterLength)|| characterLength < 8 || characterLength > 128) {
       alert("The value must be in number form, 8 - 128. Please try again.");
       return false;
     }
-
     if(confirm("Click okay if you would like lowercase letters in your password.")) {
-      choiceArr = choiceArr.concat(lowerCaseArr);
+      choiceArr = choiceArr.concat(lowercaseArr);
     }
     if(confirm("Click okay if you would like Uppercase letters in your password.")) {
-      choiceArr = choiceArr.concat(lowerCaseArr);
+      choiceArr = choiceArr.concat(uppercaseArr);
     }
     if(confirm("Click okay if you would like special characters in your password.")) {
-      choiceArr = choiceArr.concat(lowerCaseArr);
+      choiceArr = choiceArr.concat(specialCharacterArr);
     }
+    if(confirm("Click okay if you would like numbers in your password.")) {
+      choiceArr = choiceArr.concat(numberArr);
+    }
+    return true;
 }
+
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
+
     var password = generatePassword();
     var passwordText = document.querySelector("#password");
   
