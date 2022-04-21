a138710c5dea0194c169437550358f948db71b84
mattgaither <benjamin.gaither@shipt.com>
2022-04-21 18:48:43 -0500

    asking BCS for help and sending the git hub link

--- Develop/script.js
+++ Develop/script.js
@@ -1,15 +1,47 @@
 // Assignment code here
-var characterLength = 8;
 var choiceArr = [];
+var characterLength = 8;
 
 
-var lowercaseArr = Array( 26 ).fill( 1 ).map( ( _, i ) => String.fromCharCode( 97 + i ) );
-var uppercaseArr = Array( 26 ).fill( 1 ).map( ( _, i ) => String.fromCharCode( 65 + i ) );
-var numberArr = Array( 10 ).fill( 1 ).map( ( _, i ) => String.fromCharCode( 48 + i ) );
+// var lowercaseArr = Array( 26 ).fill( 1 ).map( ( _, i ) => String.fromCharCode( 97 + i ) );
+// var uppercaseArr = Array( 26 ).fill( 1 ).map( ( _, i ) => String.fromCharCode( 65 + i ) );
+// var numberArr = Array( 10 ).fill( 1 ).map( ( _, i ) => String.fromCharCode( 48 + i ) );
+var lowercaseArr = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
+var uppercaseArr =['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
+var numberArr = ['0','1','2','3','4','5','6','7','8','9'];
 var specialCharacterArr = ['@','$','%','&','#','?','!','+','=','*','<','>'];
 
+// Get references to the #generate element
+var generateBtn = document.querySelector("#generate");
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword());
+
+// Write password to the #password input
+function writePassword() {
+  var correctPromopts = getPrompt();
+  var passwordText = document.querySelector("#password");
+
+  if (correctPromopts) {
+    var newPassword = generatePassword();
+    passwordText.value = newPassword;
+  } 
+  else {
+   passwordText.value = "";
+  }
+}
+
+function generatePassword() {
+  var password = "";
+  for(var i = 0; i < characterLength; i++) {
+    var randomindex = math.floor(math.random() * choiceArr.length);
+    password = password + choiceArr[randomindex];
+  }
+  return password;
+}
+
 // prompts to generate passcode
-function getPrompts() {
+function getPrompt() {
   // This resets the choiceArr so it does not save and repaaet the function on the next generate button click
   choiceArr = [];
 
@@ -33,21 +65,3 @@ function getPrompts() {
     }
     return true;
 }
-
-
-// Get references to the #generate element
-var generateBtn = document.querySelector("#generate");
-
-// Write password to the #password input
-function writePassword() {
-
-    var password = generatePassword();
-    var passwordText = document.querySelector("#password");
-  
-    passwordText.value = password;
-
-}
-
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
- 
