af59f5415a28e173a47a08d56fecfa70a39d9cb9
mattgaither <benjamin.gaither@shipt.com>
2022-04-21 13:30:19 -0500

    added a function for the prompt

--- Develop/script.js
+++ Develop/script.js
@@ -1,15 +1,40 @@
+var characterLength = 8;
+var choiceArr = [];
+
+
+
+
 // Assignment code here
 
+// prompts to generate passcode
+function getPrompts() {
+  characterLength = parseInt(prompt("How many characters would you like your password to be? (8 - 128 characters)"));
+
+    if(isNaN(characterLength)|| characterLength < 8 || characterLength > 128) {
+      alert("The value must be in number form, 8 - 128. Please try again.");
+      return false;
+    }
+
+    if(confirm("Click okay if you would like lowercase letters in your password.")) {
+      choiceArr = choiceArr.concat(lowerCaseArr);
+    }
+    if(confirm("Click okay if you would like Uppercase letters in your password.")) {
+      choiceArr = choiceArr.concat(lowerCaseArr);
+    }
+    if(confirm("Click okay if you would like special characters in your password.")) {
+      choiceArr = choiceArr.concat(lowerCaseArr);
+    }
+}
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
-
-  passwordText.value = password;
+    var password = generatePassword();
+    var passwordText = document.querySelector("#password");
+  
+    passwordText.value = password;
 
 }
 
