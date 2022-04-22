39ac5e9edbb56d90836746f81823feb00da5e127
mattgaither <benjamin.gaither@shipt.com>
2022-04-21 19:05:11 -0500

    fix my errors and finished the challenge

--- Develop/script.js
+++ Develop/script.js
@@ -3,19 +3,16 @@ var choiceArr = [];
 var characterLength = 8;
 
 
-// var lowercaseArr = Array( 26 ).fill( 1 ).map( ( _, i ) => String.fromCharCode( 97 + i ) );
-// var uppercaseArr = Array( 26 ).fill( 1 ).map( ( _, i ) => String.fromCharCode( 65 + i ) );
-// var numberArr = Array( 10 ).fill( 1 ).map( ( _, i ) => String.fromCharCode( 48 + i ) );
-var lowercaseArr = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
-var uppercaseArr =['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
-var numberArr = ['0','1','2','3','4','5','6','7','8','9'];
+var lowercaseArr = Array( 26 ).fill( 1 ).map( ( _, i ) => String.fromCharCode( 97 + i ) );
+var uppercaseArr = Array( 26 ).fill( 1 ).map( ( _, i ) => String.fromCharCode( 65 + i ) );
+var numberArr = Array( 10 ).fill( 1 ).map( ( _, i ) => String.fromCharCode( 48 + i ) );
 var specialCharacterArr = ['@','$','%','&','#','?','!','+','=','*','<','>'];
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
 // Add event listener to generate button
-generateBtn.addEventListener("click", writePassword());
+generateBtn.addEventListener("click", writePassword);
 
 // Write password to the #password input
 function writePassword() {
@@ -34,7 +31,7 @@ function writePassword() {
 function generatePassword() {
   var password = "";
   for(var i = 0; i < characterLength; i++) {
-    var randomindex = math.floor(math.random() * choiceArr.length);
+    var randomindex = Math.floor(Math.random() * choiceArr.length);
     password = password + choiceArr[randomindex];
   }
   return password;
